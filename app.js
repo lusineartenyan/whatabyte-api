e67ffdaf7d23aeea1ce9a007c24848fa99228b02
lusineartenyan <lusine.artenyan@gmail.com>
2023-07-17 19:50:08 +0400

    bugfix

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+const indexRouter = require('./routes/index');
 const menuRouter = require("./routes/menu");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/', indexRouter);
 app.use("/menu-items", menuRouter);
 
 // catch 404 and forward to error handler
--- routes/menu.js
+++ routes/menu.js
@@ -1,43 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const menuService = require('../services/menuService').instance();
+const menuService = require("../services/menuService").instance();
 
 /* GET menu items */
-router.get('/', async function(req, res, next) {
+router.get("/", async function (req, res, next) {
   const result = await menuService.list(req.query.nameLike);
 
-  res.json(result)
+  res.json(result);
 });
 
-router.post('/', async function(req, res, next) {
+router.post("/", async function (req, res, next) {
   const result = await menuService.add(req.body);
-  req.body.id = result.insertId
+  req.body.id = result.insertId;
 
-  res.json(req.body)
+  res.json(req.body);
 });
 
-router.get('/:id', async function(req, res, next) {
+router.get("/:id", async function (req, res, next) {
   const { id } = req.params;
   const result = await menuService.get(id);
 
-  res.json(result)
+  res.json(result);
 });
 
-router.put('/:id', async function(req, res, next) {
+router.put("/:id", async function (req, res, next) {
   const { id } = req.params;
-  req.body.id = id
+  req.body.id = id;
   const result = await menuService.update(req.body);
 
-  res.json(req.body)
+  res.json(req.body);
 });
 
-router.delete('/:id',function(req, res, next) {
+router.delete("/:id", function (req, res, next) {
   const { id } = req.params;
   const result = menuService.delete(id);
-  
+
   result
-  .then(data => res.json({success : data}))
-  .catch(err => console.log(err));
+    .then((data) => res.json({ success: data }))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
